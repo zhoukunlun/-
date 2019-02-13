d22cc46b8d354d4eab636fe46808df6fbbc8d59b
zkl <0827@qq.com>
2019-02-13 19:51:06 +0800

    登录功能开发完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login'
+import Home from '@/components/Home'
+
+// 引入css样式全局控制
+
+// import '../assets/css/global.css'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
 })
